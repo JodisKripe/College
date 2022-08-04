7cd305203dac82a64521acd16501adbad8fa7eb1
Siddharth Johri <jodis@pop-os.localdomain>
2022-08-04 11:40:36 +0530

    Search algorithms. Linear and Binary

---
+++ CSIT-124/Lin_Bin_search.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+// Code to do linear search in an array
+int linear_search(int a[], int n, int ele)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == ele)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Code to do binary search in an array
+int binary_search(int a[], int n, int ele)
+{
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (a[mid] == ele)
+        {
+            return mid;
+        }
+        else if (a[mid] < ele)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+void main(){
+    int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
+    int n = sizeof(array) / sizeof(array[0]);
+    int ele;
+    printf("Enter the element to be searched: ");
+    scanf("%d", &ele);
+    int pos = linear_search(array, n, ele);
+    if (pos != -1)
+    {
+        printf("Element found at position %d\n", pos);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+    pos = binary_search(array, n, ele);
+    if (pos != -1)
+    {
+        printf("Element found at position %d\n", pos);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+
+}
