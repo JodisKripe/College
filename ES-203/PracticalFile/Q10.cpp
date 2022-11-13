89b5afa67cf262a526131c10648cc17aa97f6b09
JodisKripe <siddharthjohri15@gmail.com>
2022-11-13 22:45:23 +0530

    prac file oops updates

---
+++ ES-203/PracticalFile/Q10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+
+class Stack{
+    public:
+            Stack();
+            void push(T i);
+            T pop();
+    private:
+            int top;
+            T st[100];
+};
+
+template <class T>
+Stack<T>::Stack(){
+        top = -1;
+}
+
+template <class T>
+void Stack<T>::push(T i){
+        st[++top] = i;
+}
+
+template <class T>
+T Stack<T>::pop(){
+        return st[top--];
+}
+
+int main ()
+{
+        Stack<int> int_stack;
+        Stack<string> str_stack;
+        int_stack.push(33);
+        str_stack.push("Hello");
+        str_stack.push("Ram");
+        cout << int_stack.pop() << endl;
+        cout << str_stack.pop() << endl;
+        cout << str_stack.pop() << endl;
+        return 0;
+}
